0e79c2b551272013fa2a84ce8f5ff1c338fa125a
Pedro Henrique <95645963+Pedrohswd@users.noreply.github.com>
2023-05-27 22:12:38 -0300

    correção

--- src/br/com/login/model/Login.java
+++ src/br/com/login/model/Login.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -49,56 +48,3 @@ public class Login {
     }
 
 }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package br.com.login.model;
-
-/**
- *
- * @author felip
- */
-public class Login {
-    
-    String Email,NomeCompleto,Senha;
-
-    public Login(String Email, String Senha) {
-        this.Email = Email;
-        this.Senha = Senha;
-    }
-    
-    public void cadastrar(String Email, String NomeCompleto, String Senha  ){ 
-          this.Email = Email;
-          this.NomeCompleto = NomeCompleto;
-        this.Senha = Senha;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String Email) {
-        this.Email = Email;
-    }
-
-    public String getNomeCompleto() {
-        return NomeCompleto;
-    }
-
-    public void setNomeCompleto(String NomeCompleto) {
-        this.NomeCompleto = NomeCompleto;
-    }
-
-    public String getSenha() {
-        return Senha;
-    }
-
-    public void setSenha(String Senha) {
-        this.Senha = Senha;
-    }
-    
-    
-}
->>>>>>> c38ff79e86c6f7dc9d91bd18789562f27b726b44
