e3f6f57c1e44f41248ef283e800aa58f6dba01da
Pedrohswd <pedrohsfwd@gmail.com>
2023-05-29 19:44:10 -0300

    ajuste em parâmetros BD

--- src/com/capycar/persistence/LoginDAO.java
+++ src/com/capycar/persistence/LoginDAO.java
@@ -1,43 +1,21 @@
 package com.capycar.persistence;
 
-import java.io.FileInputStream;
+import com.capycar.connection.GastoRiderAPI;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
-import javax.swing.JOptionPane;
 
 public class LoginDAO {
 
     private Connection connection;
+    GastoRiderAPI connectionBD = new GastoRiderAPI();
 
     public LoginDAO() {
-        // Configurar a conexão com o banco de dados
-        Properties props = new Properties();
-        try {
-            FileInputStream in = new FileInputStream("src/DB/CFG_DB.txt");
-            props.load(in);
-            in.close();
-
-            String url = props.getProperty("url"); // Obtém a URL de conexão a partir do arquivo de propriedades
-            String user = props.getProperty("usuario"); // Obtém o nome de usuário do banco de dados a partir do arquivo de propriedades
-            String password = props.getProperty("senha"); // Obtém a senha do banco de dados a partir do arquivo de propriedades
-
-            connection = DriverManager.getConnection(url, user, password);
+            connection = connectionBD.conectBD();
             System.out.println("Conexão com o banco de dados estabelecida!");
-        } catch (Exception e) {
-            System.out.println("Falha na conexão com o banco de dados: " + e.getMessage());
-            e.printStackTrace();
-        }
     }
 
-    /*public void cadastrarUsuario( String Email, String NomeCompleto, String Senha) throws SQLException{
-       String sql = "INSERT INTO Usuario (email, nomecompleto, senha) VALUES( ' "+Email+" ' , ' "+NomeCompleto+" ' ,' "+Senha+" ')";
-         PreparedStatement statement = connection.prepareStatement(sql);
-         statement.execute();
-         connection.close(); */
     public boolean login(String Email, String Senha) throws SQLException {
         String sql = "SELECT email, senha FROM Usuario WHERE email = ? AND senha = ?";
         try ( PreparedStatement statement = connection.prepareStatement(sql)) {
