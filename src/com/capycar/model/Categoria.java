53234ea8f23dd88e2b6cb5ede87534981551372c
Pedro Henrique <pedrohsfwd@gmail.com>
2023-06-12 08:35:37 -0300

    tela lançamento

--- src/com/capycar/model/Categoria.java
+++ src/com/capycar/model/Categoria.java
@@ -9,36 +9,57 @@ package com.capycar.model;
  * @author educa
  */
 public class Categoria {
+
     private int id_categoria;
     private String descricao;
     private String tipo;
     private String categoria_pai;
 
-    public Categoria(){}
-    public Categoria(int id_categoria,String descricao, String tipo, String categoria_pai) {
+    public Categoria() {
+    }
+
+    public Categoria(int id_categoria, String descricao, String tipo, String categoria_pai) {
         this.id_categoria = id_categoria;
         this.descricao = descricao;
         this.tipo = tipo;
         this.categoria_pai = categoria_pai;
     }
 
-    public int getId_categoria(){return id_categoria;}
-    public void setId_categoria() {this.id_categoria = id_categoria;}
-    
-    public String getDescricao() { return descricao;}
-    public void setDescricao(String descricao) { this.descricao = descricao;}
+    public int getId_categoria() {
+        return id_categoria;
+    }
 
-    public String getTipo() { return tipo;}
-    public void setTipo(String tipo) { this.tipo = tipo;}
+    public void setId_categoria() {
+        this.id_categoria = id_categoria;
+    }
 
-    public String getCategoria() {return categoria_pai;}
+    public String getDescricao() {
+        return descricao;
+    }
 
-    public void setCategoria(String categoria) { this.categoria_pai = categoria; }
-    
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCategoria() {
+        return categoria_pai;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria_pai = categoria;
+    }
+
     @Override
     public String toString() {
-        return "Categoria{" +"id_categoria="+ id_categoria +"descricao=" + descricao + ", tipo=" + tipo + ", categoria=" + categoria_pai+'}';
+        return "Categoria{" + "id_categoria=" + id_categoria + "descricao=" + descricao + ", tipo=" + tipo + ", categoria=" + categoria_pai + '}';
     }
-    
-    
+
 }
